378803fd8fbab62ed4863ae490c5c3f31a53649b
Denys Zheleznyi <denys.zheleznyi@chisw.com>
2021-04-20 18:24:07 +0300

    main, reduce method

--- MyArray.js
+++ MyArray.js
@@ -1,23 +1,52 @@
 export class MyArray {
     constructor(...args) {
-        this.arrayObj = {}
         let index = 0
         args.forEach(argument => {
-            this.arrayObj[index] = argument
+            this[index] = argument
             index++
         })
     }
 
-    push(item) {
-        let length = this.getLength(this.arrayObj)
-        this.arrayObj[length + 1] = item
-        return this.getLength(this.arrayObj)
+    myPush(item) {
+        let length = this.getLength(this)
+        this[length + 1] = item
+        return this.getLength(this)
     }
-    pop() {
-        let length = this.getLength(this.arrayObj)
-        let lastItem = this.arrayObj[length]
-        delete this.arrayObj[length]
+
+    myPop() {
+        let length = this.getLength(this)
+        let lastItem = this[length]
+        delete this[length]
         return lastItem
+    }
+
+    myMap(func) {
+        let index = 0
+        let newArrayObj = {}
+        if (typeof func !== 'function') throw new Error('Please add function')
+        for (let item in this) {
+            newArrayObj[index] = func(this[item])
+            index++
+        }
+        return newArrayObj
+    }
+
+    myForEach(func) {
+        if (typeof func !== 'function') throw new Error('Please add function')
+        for (let item in this) {
+            this[item] = func(this[item], item)
+        }
+    }
+
+    myReduce(func, initialValue) {
+        let accumulator = initialValue || 0
+
+        if (typeof func !== 'function') throw new Error('Please add function')
+
+        for (let item in this) {
+            accumulator = func(accumulator, this[item], item, this)
+        }
+        return accumulator
     }
 
     getLength(array) {
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import {MyArray} from "./MyArray.js";
 
-const arr = new MyArray(1 ,4, [2,4], { name: 'Misha' });
-let newArr = arr.push('555')
-console.log(arr, newArr)
-let poppedArr = arr.pop()
-console.log(arr, poppedArr)
+const arr = new MyArray(1, 4, [2,4]);
 
 
+const reducer = (accumulator, currentValue) => [...accumulator, currentValue];
 
+console.log(arr.myReduce(reducer, []))
 
